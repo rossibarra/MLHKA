d4a687bfc36cd688aa94ba528998e76b1ab47708
Jeff Ross-Ibarra <rossibarra@ucdavis.edu>
2013-06-16 00:45:19 -0700

    edited dumb copyright statement

--- MLHKA_version2.1.cpp
+++ MLHKA_version2.1.cpp
@@ -1,9 +1,8 @@
 
 /******************************************************************************
- *  Copyright (C) 2004-2010 Stephen Wright <stephen.wright@utoronto.ca>                          *
- *  Portions of the code (C) 2010 Jeffrey Ross-Ibarra <rossibarra@ucdavis.edu>   *
+ *  Copyright (C) 2004-2010 Stephen Wright <stephen.wright@utoronto.ca>         *
  *                                                                              *
- *  This program is free software: you can redistribute it and/or modify                *
+ *  This program is free software: you can redistribute it and/or modify        *
  *  it under the terms of the GNU General Public License as published by        *
  *  the Free Software Foundation, either version 3 of the License, or           *
  *  (at your option) any later version.                                         *
